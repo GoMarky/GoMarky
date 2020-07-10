c31e261298bff7c9fc2bf65788cb208e225769fb
TeodorDre <swen295@gmail.com>
2020-07-11 01:53:47 +0300

    feat(core): testing cpp code

--- source/foo.cpp
+++ source/foo.cpp
@@ -2,13 +2,9 @@
 #include "foo.h"
 #include <fmt/format.h>
 
-
 int foo(bool branch)
 {
-    if(branch)
-    {
-        fmt::print("This line will be untested, so that coverage is not 100%\n");
-    }
+    if (branch) { fmt::print("This line will be untested, so that coverage is not 100%\n"); }
     else
     {
         fmt::print("This is the default behaviour and will be tested\n");
--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,39 @@
 #include <fmt/format.h>
+#include "iostream"
 
-// This should be in the headers
+using namespace std;
 
-int foo();
-
-int main(int argc, char* argv[])
+constexpr double nth(double x, int n)
 {
-    if (argc)
+    double res = 1;
+    int    i   = 0;
+
+    while (i < n)
     {
-        fmt::print("hello world from !", argv[0]);
+        res *= x;
+        ++i;
     }
-    return 0;
+
+    return res;
 }
 
-// Implementation
-int foo() { return 42; }
+static void copy_fct()
+{
+    int v1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    int v2[10];
+
+    for (int i = 0; i != 10; ++i)
+    {
+        v2[i] = v1[i];
+    }
+}
+
+int main()
+{
+    constexpr double s2 = nth(5, 5);
+
+    fmt::print("This is the default behaviour and will be tested");
+
+    return 0;
+};
