59aa2e6749454c3700617ec38b948ccb3cb47158
TeodorDre <swen295@gmail.com>
2020-08-17 18:15:44 +0300

    feat(layout): testing layout with calculator example

---
+++ source/code/calculator/calculator.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Andrew Slesarenko on 17/08/2020.
+//
+#include "calculator.h"
+#include "QGridLayout"
+
+Calculator::Calculator(QWidget* pwgt) : QWidget(pwgt)
+{
+    m_plcd = new QLCDNumber(12);
+    m_plcd->setSegmentStyle(QLCDNumber::Flat);
+    m_plcd->setMinimumSize(150, 50);
+
+    QChar aButtons[4][4] = {
+        {'7', '8', '9', '/'}, {'4', '5', '6', '*'}, {'1', '2', '3', '-'}, {'0', '.', '=', '+'}};
+
+    QGridLayout* ptopLayout = new QGridLayout();
+
+    ptopLayout->addWidget(m_plcd, 0, 0, 1, 4);
+    ptopLayout->addWidget(createButton("CE"), 1, 3);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            ptopLayout->addWidget(createButton(aButtons[i][j]), i + 2, j);
+        }
+    }
+
+    setLayout(ptopLayout);
+}
+
+QPushButton* Calculator::createButton(const QString& str)
+{
+    QPushButton* pcmd = new QPushButton(str);
+
+    pcmd->setMinimumSize(40, 40);
+
+    connect(pcmd, SIGNAL(clicked()), SLOT(slotButtonClicked()));
+
+    return pcmd;
+}
+
+void Calculator::calculate()
+{
+    qreal   fOperand2    = m_stk.pop().toFloat();
+    QString strOperation = m_stk.pop();
+    qreal   fOperand1    = m_stk.pop().toFloat();
+
+    qreal fResult = 0;
+
+    if (strOperation == "+") { fResult = fOperand1 + fOperand2; }
+
+    if (strOperation == "-") { fResult = fOperand1 - fOperand2; }
+
+    if (strOperation == "/") { fResult = fOperand1 / fOperand2; }
+
+    if (strOperation == "*") { fResult = fOperand1 * fOperand2; }
+
+    m_plcd->display(fResult);
+}
+
+void Calculator::slotButtonClicked() {
+
+}
--- source/main.cpp
+++ source/main.cpp
@@ -3,51 +3,22 @@
  */
 // The previous block is needed in every file for which you want to generate documentation
 
-#include <fmt/format.h>
-#include "counter/counter.h"
 #include "QtWidgets"
 
-#include "iostream"
+#include "code/calculator/calculator.h"
 
-// This should be in the headers
-
-/**
- * @brief A function that does nothing but generate documentation
- * @return The answer to life, the universe and everything
- */
-int foo();
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    QLabel      label("0");
-    QPushButton command("ADD");
+    Calculator calculator;
 
-    if (label.inherits("QWidget")) {
-        std::cout << "Label inherits from 'QWidget'";
-    }
+    calculator.setWindowTitle("Calculator");
 
-    Counter counter;
+    calculator.resize(230, 200);
+    calculator.show();
 
-    label.show();
-
-    QRect labelRect(0, 0, 250, 250);
-
-    label.setGeometry(labelRect);
-
-    label.setWindowTitle("Gomarky window");
-
-    command.show();
-
-    QObject::connect(&command, SIGNAL(clicked()), &counter, SLOT(slotInc()));
-
-    QObject::connect(&counter, SIGNAL(counterChanged(int)), &label, SLOT(setNum(int)));
-
-    QObject::connect(&counter, SIGNAL(goodbye()), &app, SLOT(quit()));
-
-    return QApplication::exec();
+    return app.exec();
 };
 
-// Implementation
-int foo() { return 42; }
